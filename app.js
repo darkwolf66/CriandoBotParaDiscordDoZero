c1ea6b734249138336b2ff5039a17d4af9f5be6a
Darkwolf_66 <Darkwolf_66>
2018-11-03 18:33:45 -0300

    Modulo #1 - Aula 4

--- app.js
+++ app.js
@@ -2,29 +2,34 @@ const Discord = require('discord.js');
 
 var bot = new Discord.Client();
 
-
 const prefixoDeComando = "/";
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-
-
+// Listener Mensagens: É disparado sempre que uma mensagem é recebida em algum lugar
 bot.on('message', msg => {
 
     let autor = msg.author
-    let mensagem = msg.content.toUpperCase()
+    let conteudoDaMensagem = msg.content
 
-    if(mensagem === prefixoDeComando + 'PING'){
-        msg.channel.send('Pong');
+    // Comando de Ping
+    if(conteudoDaMensagem.toUpperCase() === prefixoDeComando + 'PING'){
+        msg.channel.send('Pong')
     }
+
+    if(msg.channel.id = "508379525579800578"){
+        if(isNaN(conteudoDaMensagem) && !msg.author.bot){
+            msg.delete().then(() => {
+                msg.channel.send('Sua mensagem foi deletada.. Só é possivel enviar numeros neste canal!')
+                    .then(mensagemEnviada => setTimeout(() => mensagemEnviada.delete(), 5000))
+            })
+        }
+    }
+
 
 })
 
 
-
-
-
-
-bot.login('NTA4MzY1Mjg3NjQxNDQ4NDQ5.Dr-LsQ.NpAtR1ySL-J7bHlE8oC5dsUbOy4');
+bot.login(require('./env.js').token);
