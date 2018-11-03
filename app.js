d2bdf5ea4c04de4e155c71c214bf9330140a4f09
Darkwolf_66 <Darkwolf_66>
2018-11-03 17:08:24 -0300

    Primeiro código do bot -> Comando Ping

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("O nodejs está funcionando!");
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+
+
+const prefixoDeComando = "/";
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+
+
+bot.on('message', msg => {
+
+    let autor = msg.author
+    let mensagem = msg.content.toUpperCase()
+
+    if(mensagem === prefixoDeComando + 'PING'){
+        msg.channel.send('Pong');
+    }
+
+})
+
+
+
+
+
+
+bot.login('NTA4MzY1Mjg3NjQxNDQ4NDQ5.Dr-LsQ.NpAtR1ySL-J7bHlE8oC5dsUbOy4');
